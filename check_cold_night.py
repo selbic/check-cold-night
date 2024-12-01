b0e1235456fe150a594507b33002fcdcd9193aed
Selbo <sebastian.eppner@gmail.com>
2024-12-01 23:30:33 +0100

    Initial commit for cold night checker

---
+++ check_cold_night.py
@@ -0,0 +1,56 @@
+import requests
+from datetime import datetime, timedelta
+import pytz
+
+# OpenWeatherMap API-Konfiguration
+API_KEY = 'c612438a64c0fe7aa0de7472326891a1'  # Ersetze mit deinem API-Schlüssel
+LOCATION = 'Berlin,de'                  # Stadt und Land
+UNITS = 'metric'                        # Metrische Einheiten (°C)
+WEATHER_URL = f'http://api.openweathermap.org/data/2.5/forecast?q={LOCATION}&appid={API_KEY}&units={UNITS}'
+
+# Pushbullet API-Konfiguration
+PUSHBULLET_API_TOKEN = 'o.w7A73dZfWP1wwQFXojhAn2ug8zFalppP'  # Ersetze mit deinem API-Token
+PUSHBULLET_URL = 'https://api.pushbullet.com/v2/pushes'
+
+def get_weather_forecast():
+    """Ruft die Wettervorhersage von OpenWeatherMap ab."""
+    response = requests.get(WEATHER_URL)
+    response.raise_for_status()
+    return response.json()
+
+def send_push_notification(title, message):
+    """Sendet eine Push-Benachrichtigung über Pushbullet."""
+    headers = {
+        'Access-Token': PUSHBULLET_API_TOKEN,
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'type': 'note',
+        'title': title,
+        'body': message
+    }
+    response = requests.post(PUSHBULLET_URL, headers=headers, json=data)
+    response.raise_for_status()
+
+def check_for_cold_night():
+    """Überprüft die Wettervorhersage auf Temperaturen unter -5°C in der kommenden Nacht."""
+    forecast = get_weather_forecast()
+    timezone = pytz.timezone('Europe/Berlin')  # Zeitzone für Deutschland
+    current_time = datetime.now(timezone)
+    next_night_start = (current_time + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+
+    for item in forecast['list']:
+        forecast_time = datetime.fromtimestamp(item['dt'], tz=timezone)
+        if next_night_start <= forecast_time < next_night_start + timedelta(hours=6):  # Nachtstunden (00:00 - 06:00)
+            temp = item['main']['temp']
+            if temp < 100:
+                send_push_notification(
+                    title="Kalte Nachtwarnung!",
+                    message=f"In der kommenden Nacht wird es {temp}°C kalt. Bring die Pflanzen rein!"
+                )
+                break
+
+if __name__ == '__main__':
+    check_for_cold_night()
+
+
